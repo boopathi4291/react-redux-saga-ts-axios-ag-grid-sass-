bfae261a80ea0d7f649c6ac3b89f7d70df67daa2
boopathi4291 <boopathi.career@live.com>
2018-08-08 00:24:09 +0530

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -2,40 +2,15 @@ import * as React from 'react';
 import './styles/App.css';
 
 import MainComponent from './components/main.component';
-import { connect } from "react-redux";
-import { Dispatch } from 'redux';
-import { bindActionCreators } from 'redux';
-import { getAllRecords, updateRecord } from './actions/actions';
 
 
-class App extends React.Component<any, any> {
-
-  constructor(props: any) {
-    super(props);
-    this.state = { myData: this.props.myData }
-  }
-
-
-
-  public render() {
-
+function App()  {
     return (
       <div className="App">
-        <MainComponent myProps={this.props} />
+        <MainComponent />
       </div>
     );
-  }
 }
 
-// const mapStateToProps = (state: any) => {
-//   return {
-
-//   };
-// };
-
-const mapDispatchToProps = (dispatch: Dispatch) => {
-  return bindActionCreators({ getAllRecords, updateRecord }, dispatch)
-};
-
-export default connect(null, mapDispatchToProps)(App);
+export default App;
 
